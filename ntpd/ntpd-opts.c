139293b8fd0eaa2980ebfbded5df0e13ebeeacc7
stenn <stenn@psp-deb1.ntp.org>
2017-05-19 12:38:05 +0000

    autogen updates
    bk: 591ee72d6USjc3jauIBLMgb0HHu_Zw

--- ntpd/ntpd-opts.c
+++ ntpd/ntpd-opts.c
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (ntpd-opts.c)
  *
- *  It has been AutoGen-ed  March 21, 2017 at 10:42:12 AM by AutoGen 5.18.5
+ *  It has been AutoGen-ed  May 19, 2017 at 12:32:23 PM by AutoGen 5.18.5
  *  From the definitions    ntpd-opts.def
  *  and the template file   options
  *
