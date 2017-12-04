f26bee85a7e31e7ee4c7eae14c868e8e31331355
harlan <harlan@psp-at0.tal1.ntfo.org>
2017-12-04 00:24:49 -0800

    record_raw_stats(): Log entire packet.  Log writes.
    bk: 5a250651bqz2hlj7ni1ji_msyYW61w

--- ntpd/ntp_io.c
+++ ntpd/ntp_io.c
@@ -3091,6 +3091,7 @@ sendpkt(
 	int	cc;
 	int	rc;
 	u_char	cttl;
+	l_fp	fp_zero = { 0, 0 };
 
 	ismcast = IS_MCAST(dest);
 	if (!ismcast)
@@ -3174,6 +3175,18 @@ sendpkt(
 		if (ismcast)
 			src = src->mclink;
 	} while (ismcast && src != NULL);
+
+	record_raw_stats(&src->sin, dest,
+			&pkt->org, &pkt->rec, &pkt->xmt, &fp_zero,
+			PKT_MODE(pkt->li_vn_mode),
+			PKT_VERSION(pkt->li_vn_mode),
+			PKT_LEAP(pkt->li_vn_mode),
+			pkt->stratum,
+			pkt->ppoll, pkt->precision,
+			pkt->rootdelay, pkt->rootdisp, pkt->refid,
+			len - MIN_V4_PKT_LEN, (u_char *)&pkt->exten);
+
+	return;
 }
 
 
