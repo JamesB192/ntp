967c3ffbfdbdceba864a317c02eefdf39d770612
stenn <stenn@psp-deb1.ntp.org>
2017-12-11 08:52:38 +0000

    merge cleanup
    bk: 5a2e4756oSthTJSU5DtoUZor8ZG9RQ

--- sntp/sntp-opts.h
+++ sntp/sntp-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (sntp-opts.h)
  *
- *  It has been AutoGen-ed  May 19, 2017 at 12:30:19 PM by AutoGen 5.18.5
+ *  It has been AutoGen-ed  December 11, 2017 at 08:51:08 AM by AutoGen 5.18.5
  *  From the definitions    sntp-opts.def
  *  and the template file   options
  *
