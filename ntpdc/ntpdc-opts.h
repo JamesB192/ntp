caa65616530c86c41a9cc70dfc6c02445c010248
ntpreleng <ntpreleng@ntp-build.tal1.ntfo.org>
2019-04-08 22:11:36 -0700

    NTP_4_3_95
    bk: 5cac2988leus-hzG6pNGubKHJJGK-Q

--- ntpdc/ntpdc-opts.h
+++ ntpdc/ntpdc-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (ntpdc-opts.h)
  *
- *  It has been AutoGen-ed  April  7, 2019 at 03:00:32 AM by AutoGen 5.18.5
+ *  It has been AutoGen-ed  April  8, 2019 at 10:11:06 PM by AutoGen 5.18.5
  *  From the definitions    ntpdc-opts.def
  *  and the template file   options
  *
@@ -83,9 +83,9 @@ typedef enum {
 /** count of all options for ntpdc */
 #define OPTION_CT    15
 /** ntpdc version */
-#define NTPDC_VERSION       "4.3.94"
+#define NTPDC_VERSION       "4.3.95"
 /** Full ntpdc version text */
-#define NTPDC_FULL_VERSION  "ntpdc 4.3.94"
+#define NTPDC_FULL_VERSION  "ntpdc 4.3.95"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
--- ntpq/ntpq-opts.h
+++ ntpq/ntpq-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (ntpq-opts.h)
  *
- *  It has been AutoGen-ed  April  7, 2019 at 03:00:38 AM by AutoGen 5.18.5
+ *  It has been AutoGen-ed  April  8, 2019 at 10:11:13 PM by AutoGen 5.18.5
  *  From the definitions    ntpq-opts.def
  *  and the template file   options
  *
@@ -84,9 +84,9 @@ typedef enum {
 /** count of all options for ntpq */
 #define OPTION_CT    16
 /** ntpq version */
-#define NTPQ_VERSION       "4.3.94"
+#define NTPQ_VERSION       "4.3.95"
 /** Full ntpq version text */
-#define NTPQ_FULL_VERSION  "ntpq 4.3.94"
+#define NTPQ_FULL_VERSION  "ntpq 4.3.95"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
