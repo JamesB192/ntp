924d00e9b8bd551c44bfe65b5e2866230350c042
ntpreleng <ntpreleng@ntp-build.tal1.ntfo.org>
2023-05-31 20:05:32 -0700

    NTP_4_2_8P16
    bk: 64780afcLISj0j3gETjtoCMlygEMCQ

--- ntpsnmpd/ntpsnmpd-opts.h
+++ ntpsnmpd/ntpsnmpd-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (ntpsnmpd-opts.h)
  *
- *  It has been AutoGen-ed  March 18, 2023 at 11:21:54 AM by AutoGen 5.18.16
+ *  It has been AutoGen-ed  May 31, 2023 at 02:50:10 PM by AutoGen 5.18.16
  *  From the definitions    ntpsnmpd-opts.def
  *  and the template file   options
  *
@@ -84,9 +84,9 @@ typedef enum {
 /** count of all options for ntpsnmpd */
 #define OPTION_CT    8
 /** ntpsnmpd version */
-#define NTPSNMPD_VERSION       "4.2.8p15"
+#define NTPSNMPD_VERSION       "4.2.8p16"
 /** Full ntpsnmpd version text */
-#define NTPSNMPD_FULL_VERSION  "ntpsnmpd 4.2.8p15"
+#define NTPSNMPD_FULL_VERSION  "ntpsnmpd 4.2.8p16"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
