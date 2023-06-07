a11053b773aa6b81c5f21d0e142cdab955ddf13b
ntpreleng <ntpreleng@ntp-build.tal1.ntfo.org>
2023-06-07 02:14:19 -0700

    NTP_4_3_103
    bk: 64804a6by6hxd_ocCqfBB030hde8eg

--- ntpdc/ntpdc-opts.h
+++ ntpdc/ntpdc-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (ntpdc-opts.h)
  *
- *  It has been AutoGen-ed  June  1, 2023 at 01:29:40 AM by AutoGen 5.18.16
+ *  It has been AutoGen-ed  June  7, 2023 at 02:11:45 AM by AutoGen 5.18.16
  *  From the definitions    ntpdc-opts.def
  *  and the template file   options
  *
@@ -92,9 +92,9 @@ typedef enum {
 /** count of all options for ntpdc */
 #define OPTION_CT    16
 /** ntpdc version */
-#define NTPDC_VERSION       "4.3.102"
+#define NTPDC_VERSION       "4.3.103"
 /** Full ntpdc version text */
-#define NTPDC_FULL_VERSION  "ntpdc 4.3.102"
+#define NTPDC_FULL_VERSION  "ntpdc 4.3.103"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
