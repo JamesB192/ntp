690199d56657c83f5b8dd30d2a038fa1e2910092
harlan <harlan@ntp-testbuild.tal1.ntfo.org>
2024-01-07 00:46:19 -0800

    Makefile.am:   Added util/lsf-times Many files:   Update copyright year to 2024
    bk: 659a64dbAcZW0TuDul-Z6DJt22F-6w

--- sntp/sntp-opts.c
+++ sntp/sntp-opts.c
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (sntp-opts.c)
  *
- *  It has been AutoGen-ed  June  6, 2023 at 04:35:52 AM by AutoGen 5.18.16
+ *  It has been AutoGen-ed  January  7, 2024 at 12:33:32 AM by AutoGen 5.18.16
  *  From the definitions    sntp-opts.def
  *  and the template file   options
  *
@@ -18,7 +18,7 @@
  * The sntp program is copyrighted and licensed
  * under the following terms:
  *
- *  Copyright (C) 1992-2023 The University of Delaware and Network Time Foundation, all rights reserved.
+ *  Copyright (C) 1992-2024 The University of Delaware and Network Time Foundation, all rights reserved.
  *  This is free software. It is licensed for use, modification and
  *  redistribution under the terms of the NTP License, copies of which
  *  can be seen at:
@@ -75,7 +75,7 @@ extern FILE * option_usage_fp;
  */
 static char const sntp_opt_strs[2567] =
 /*     0 */ "sntp 4.2.8p17\n"
-            "Copyright (C) 1992-2023 The University of Delaware and Network Time Foundation, all rights reserved.\n"
+            "Copyright (C) 1992-2024 The University of Delaware and Network Time Foundation, all rights reserved.\n"
             "This is free software. It is licensed for use, modification and\n"
             "redistribution under the terms of the NTP License, copies of which\n"
             "can be seen at:\n"
@@ -1181,7 +1181,7 @@ static void bogus_function(void) {
    */
   /* referenced via sntpOptions.pzCopyright */
   puts(_("sntp 4.2.8p17\n\
-Copyright (C) 1992-2023 The University of Delaware and Network Time Foundation, all rights reserved.\n\
+Copyright (C) 1992-2024 The University of Delaware and Network Time Foundation, all rights reserved.\n\
 This is free software. It is licensed for use, modification and\n\
 redistribution under the terms of the NTP License, copies of which\n\
 can be seen at:\n"));
