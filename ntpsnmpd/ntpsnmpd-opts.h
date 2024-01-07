12b433f35c539965add99e950b792fed25327a2b
harlan <harlan@ntp-testbuild.tal1.ntfo.org>
2024-01-07 00:46:19 -0800

    Makefile.am:   Added util/lsf-times Many files:   Update copyright year to 2024
    bk: 659a64dbAcZW0TuDul-Z6DJt22F-6w

--- ntpsnmpd/ntpsnmpd-opts.h
+++ ntpsnmpd/ntpsnmpd-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (ntpsnmpd-opts.h)
  *
- *  It has been AutoGen-ed  June  6, 2023 at 04:38:16 AM by AutoGen 5.18.16
+ *  It has been AutoGen-ed  January  7, 2024 at 12:36:04 AM by AutoGen 5.18.16
  *  From the definitions    ntpsnmpd-opts.def
  *  and the template file   options
  *
@@ -18,7 +18,7 @@
  * The ntpsnmpd program is copyrighted and licensed
  * under the following terms:
  *
- *  Copyright (C) 1992-2023 The University of Delaware and Network Time Foundation, all rights reserved.
+ *  Copyright (C) 1992-2024 The University of Delaware and Network Time Foundation, all rights reserved.
  *  This is free software. It is licensed for use, modification and
  *  redistribution under the terms of the NTP License, copies of which
  *  can be seen at:
