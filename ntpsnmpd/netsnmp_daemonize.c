df42651e496bd76e1355322405b37f3a583e85b9
davehart <davehart@tl.ntp.md>
2024-04-12 12:23:49 +0000

    Update libtool initialization macros to recommended. Quiet some more Autoconf deprecation warnings.
    bk: 661927d5ob0e-w_tWoHMdRuuU7t4XQ

--- ntpsnmpd/netsnmp_daemonize.c
+++ ntpsnmpd/netsnmp_daemonize.c
@@ -57,20 +57,12 @@ SOFTWARE.
 #include <stdlib.h>
 #endif
 
-#if TIME_WITH_SYS_TIME
-# ifdef WIN32
-#  include <sys/timeb.h>
-# else
-#  include <sys/time.h>
-# endif
-# include <time.h>
+#ifdef WIN32
+# include <sys/timeb.h>
 #else
-# if HAVE_SYS_TIME_H
-#  include <sys/time.h>
-# else
-#  include <time.h>
-# endif
+# include <sys/time.h>
 #endif
+#include <time.h>
 
 #include <sys/types.h>
 
